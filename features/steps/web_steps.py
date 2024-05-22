edfc4782ad230ae48459124472813cb1231211a7
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-05-22 15:46:07 +0300

    Update web_steps.py
    features/steps/web_steps.py showing the Step Definition for verifying a specific message is present

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -127,6 +127,17 @@ def step_impl(context, name):
     element = context.driver.find_element_by_id('search_results')
     assert(name not in element.text)
 
+# verifying a specific message is present
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
