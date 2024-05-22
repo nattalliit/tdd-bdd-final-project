3ee84a6985136991621944ae0b82c3b4bac50a67
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-05-22 15:26:30 +0300

    Update test_routes.py
    tests/test_routes.py showing the code snippet for LIST BY NAME test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -205,6 +205,22 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertEqual(len(data), 5)
 
+    #   tests/test_routes.py showing the code snippet for LIST BY NAME test case
+            def test_query_by_name(self):
+        """It should Query Products by name"""
+        products = self._create_products(5)
+        test_name = products[0].name
+        name_count = len([product for product in products if product.name == test_name])
+        response = self.client.get(
+            BASE_URL, query_string=f"name={quote_plus(test_name)}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), name_count)
+        # check the data just to be sure
+        for product in data:
+            self.assertEqual(product["name"], test_name)
+
         # update the product
         new_product = response.get_json()
         new_product["description"] = "unknown"
