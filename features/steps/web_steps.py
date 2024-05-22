92dc17a499657bc9ec10b9ae63bdec5cfbd86e5d
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-05-22 15:44:38 +0300

    Update web_steps.py
    features/steps/web_steps.py showing the Step Definition for verifying for a specific name or text NOT to be present

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -121,7 +121,11 @@ def step_impl(context, name):
     )
     assert(found)
 
-# 
+# Specific name or text NOT to be present
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
 
 ##################################################################
 # This code works because of the following naming convention:
