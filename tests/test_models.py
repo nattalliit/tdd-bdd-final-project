64f4f75a5c080c0d58a037e81049fa20514bf93a
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-05-22 14:54:02 +0300

    Update test_models.py
    tests/test_models.py showing the code snippet for FIND BY NAME test case

--- tests/test_models.py
+++ tests/test_models.py
@@ -157,3 +157,17 @@ class TestProductModel(unittest.TestCase):
         # See if we get back 5 products
         products = Product.all()
         self.assertEqual(len(products), 5)
+
+    # tests/test_models.py showing the code snippet for FIND BY NAME test case
+         def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+    
