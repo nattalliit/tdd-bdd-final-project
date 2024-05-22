8881749dcf24ad9bdcf39dcda1dd0780316a3892
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-05-22 15:19:12 +0300

    Update test_routes.py
    tests/test_routes.py showing the code snippet for UPDATE test case

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,6 +173,22 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], test_product.name)
 
+    #  tests/test_routes.py showing the code snippet for UPDATE test case
+      def test_update_product(self):
+        """It should Update an existing Product"""
+        # create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the product
+        new_product = response.get_json()
+        new_product["description"] = "unknown"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["description"], "unknown")
+
     ######################################################################
     # Utility functions
     ######################################################################
