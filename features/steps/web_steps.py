44f49de1f294d9262496ed0624a8192ccda731fe
nattalliit <91556919+nattalliit@users.noreply.github.com>
2024-05-22 15:43:47 +0300

    Update web_steps.py
    features/steps/web_steps.py showing the Step Definition for verifying for a specific name or text to be present

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -110,6 +110,17 @@ def step_impl(context, button):
     button_id = button.lower() + '-btn'
     context.driver.find_element_by_id(button_id).click()
 
+# Specific name or text to be present
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
+
 # 
 
 ##################################################################
